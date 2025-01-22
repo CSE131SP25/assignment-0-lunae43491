7d1f5b92c093e029c9dd4d98ef62911bd814a0f6
lunae43491 <e.a.luna@wustl.edu>
2025-01-22 13:24:26 -0600

    finished

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,8 +30,13 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(60);
+		robot.forward(60);
 		robot.turnLeft(90);
 		robot.forward(23);
+		robot.turnRight(90);
+		robot.forward(80);
+		robot.turnRight(90);
+		robot.forward(120);
 		robot.turnRight(90);
 		robot.forward(80);
 	}
